5fbe158d562eb999791e52245cb0545baa0c0348
Jesús González <jesusalfonsoeslava@gmail.com>
2025-03-27 06:23:34 -0500

    Correccion de error de cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
-const app = express();
+import cors from 'cors';
 import 'dotenv/config';
 import routers from './routes/index';
 import errorHandler from './middlewares/errorHandler.middleware';
 import fileUpload from 'express-fileupload';
 
+const app = express();
+
 // Middlewares
+app.use(
+  cors({
+    origin: '*', // Permite solicitudes desde cualquier origen. Cambia esto si necesitas restringirlo.
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization', 'token'], // Incluye 'token' como encabezado permitido
+  })
+);
 app.use(express.json());
 app.use(fileUpload());
 
